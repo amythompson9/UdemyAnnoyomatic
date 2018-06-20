1cc96370a6e293a4f28624a40f1ec8e1d91ab8db
Amy Thompson <amythompson9@gmail.com>
2018-06-20 19:40:49 -0400

    Index Of

--- UdemyAnnoyomatic/JavaScript.js
+++ UdemyAnnoyomatic/JavaScript.js
@@ -1,6 +1,14 @@
 ﻿var answer = prompt("Are we there yet?");
 
-while (answer !== "yes") {
-    var answer = prompt("Are we there yet?");
+//while (answer !== "yes" && answer !== "yeah") {
+//    var answer = prompt("Are we there yet?");
+//}
+//alert("Yay! We made it!")
+
+//VERSION 2
+//use index of to see if answer contains yes or yeah
+
+while (answer.indexOf("yes") === -1 && answer.indexOf("yeah") === -1) {
+   var answer = prompt("Are we there yet?");
 }
 alert("Yay! We made it!")
